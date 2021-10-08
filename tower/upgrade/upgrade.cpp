f120604d06a16ed787e91c62cbe5bd26af00e437
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-08 22:14:20 +0800

    Implement upgrade.h methods

---
+++ tower/upgrade/upgrade.cpp
@@ -0,0 +1,26 @@
+#include "upgrade.h"
+
+Upgrade::Upgrade(int cost, int pierceIncrease, double cooldownDecrease,
+	int damageIncrease, int projectileIncrease) {
+	this->cost = cost;
+	this->pierceIncrease = pierceIncrease;
+	this->cooldownDecrease = cooldownDecrease;
+	this->damageIncrease = damageIncrease;
+	this->projectileIncrease = projectileIncrease;
+}
+
+Upgrade Upgrade::ofPierceUpgrade(int cost, int pierceIncrease) {
+	return Upgrade(cost, pierceIncrease, 1.0, 0, 0);
+}
+
+Upgrade Upgrade::ofCooldownUpgrade(int cost, double cooldownDecrease) {
+	return Upgrade(cost, 0, cooldownDecrease, 0, 0);
+}
+
+Upgrade Upgrade::ofDamageUpgrade(int cost, double damageIncrease) {
+	return Upgrade(cost, 0, 1.0, damageIncrease, 0);
+}
+
+Upgrade Upgrade::ofProjectileUpgrade(int cost, double projectileIncrease) {
+	return Upgrade(cost, 0, 1.0, 0, projectileIncrease);
+}
