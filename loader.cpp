b200ab787be182c78b1aa56d2b2bb697f0d0e604
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-10 20:34:32 +0800

    Populate loader with towers

--- loader.cpp
+++ loader.cpp
@@ -1,5 +1,8 @@
 #include "loader.h"
 
 std::vector<Tower> loadTowers() {
-
+	Tower dartMonkey = Tower(std::string("Dart Monkey"), 200, 0.95, 2);
+	Tower boomerangMonkey = Tower(std::string("Boomerang Monkey"), 325, 1.2, 4);
+	Tower tackShooter = Tower(std::string("Tack Shooter"), 280, 1.4, 1, 1, 8);
+	std::vector<Tower> TOWERS{ dartMonkey, boomerangMonkey, tackShooter };
 }
