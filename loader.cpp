731b3061715ae7d7af327b9204da99f116fcaa7f
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-10 22:54:28 +0800

    Convert towers to use new format

--- loader.cpp
+++ loader.cpp
@@ -1,32 +1,29 @@
 #include "loader.h"
-#include "tower/towerbuilder.h"
+#include "tower/tower.h"
+#include "tower/attackbuilder.h"
 
 std::vector<Tower> loadTowers() {
-	Tower dartMonkey = TowerBuilder()
-		.name("Dart Monkey")
-		.cost(200)
+	Attack dartAttack = AttackBuilder()
 		.cooldown(0.95)
 		.pierce(2)
 		.build();
-	Tower boomerangMonkey = TowerBuilder()
-		.name("Boomerang Monkey")
-		.cost(325)
+	Tower dartMonkey = Tower("Dart Monkey", 200, dartAttack);
+	Attack boomerangAttack = AttackBuilder()
 		.cooldown(1.2)
 		.pierce(4)
 		.build();
-	Tower bombShooter = TowerBuilder()
-		.name("Bomb Shooter")
-		.cost(600)
+	Tower boomerangMonkey = Tower("Boomerang Monkey", 325, boomerangAttack);
+	Attack bombAttack = AttackBuilder()
 		.cooldown(1.4)
 		.pierce(14)
 		.build();
-	Tower tackShooter = TowerBuilder()
-		.name("Tack Shooter")
-		.cost(280)
+	Tower bombShooter = Tower("Bomb Shooter", 600, bombAttack);
+	Attack tackAttack = AttackBuilder()
 		.cooldown(1.4)
 		.pierce(1)
 		.projectiles(8)
 		.build();
+	Tower tackShooter = Tower("Tack Shooter", 280, tackAttack);
 	std::vector<Tower> towers{ dartMonkey, boomerangMonkey, bombShooter, tackShooter };
 	return towers;
 }
