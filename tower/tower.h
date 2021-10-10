4d6a30661b96554dcb1b08b0602c6b6589741cfe
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-10 19:15:37 +0800

    Resolve weird git bug 2/2

---
+++ tower/tower.h
@@ -0,0 +1,24 @@
+#ifndef TOWER
+#define TOWER
+#include <string>
+#include <vector>
+#include "difficulty.h"
+class Tower {
+private:
+	std::string name;
+	int cost;
+	double cooldown; // Attack speed
+	int pierce;
+	int damage;
+	int numProjectiles;
+public:
+	Tower(std::string name, int cost, double cooldown, int pierce, int damage = 1, int numProjectiles = 1);
+	double getDamagePerSecond();
+	double getSingleTargetDps();
+	int getCost();
+	int getCost(Difficulty diff);
+	std::string getName();
+	std::string getStats();
+};
+extern std::vector<Tower> TOWERS;
+#endif
