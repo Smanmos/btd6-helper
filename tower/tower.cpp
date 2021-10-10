62dd4e7850546008a8871b463d584b213e669c1f
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-10 17:53:02 +0800

    Add Tack Shooter

--- tower/tower.cpp
+++ tower/tower.cpp
@@ -41,4 +41,5 @@ std::string Tower::getStats() {
 
 Tower dartMonkey = Tower(std::string("Dart Monkey"), 200, 0.95, 2);
 Tower boomerangMonkey = Tower(std::string("Boomerang Monkey"), 325, 1.2, 4);
+Tower tackShooter = Tower(std::string("Tack Shooter"), 280, 1.4, 1, 1, 8);
 std::vector<Tower> TOWERS{dartMonkey, boomerangMonkey};
