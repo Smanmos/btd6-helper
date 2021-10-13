4c29e7a5ed229514d4ef36ce7070a68417708611
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-13 18:26:04 +0800

    Add exception handler to Loader

--- loader.cpp
+++ loader.cpp
@@ -3,17 +3,24 @@
 #include "tower/attackbuilder.h"
 #include "nlohmann/json.hpp"
 #include <fstream>
+#include <iostream>
 
 using json = nlohmann::json;
 
 std::vector<Tower> loadTowers() {
 	std::vector<Tower> towers = std::vector<Tower>();
 	std::fstream towerFile("resources/towers.json");
-	json towerJsons;
-	towerFile >> towerJsons;
-	for (json::iterator towerIt = towerJsons.begin(); towerIt != towerJsons.end(); ++towerIt) {
-		Tower tower = Tower(towerIt.key(), towerIt.value());
-		towers.push_back(tower);
+	try {
+		json towerJsons;
+		towerFile >> towerJsons;
+		for (json::iterator towerIt = towerJsons.begin(); towerIt != towerJsons.end(); ++towerIt) {
+			Tower tower = Tower(towerIt.key(), towerIt.value());
+			towers.push_back(tower);
+		}
+	}
+	catch (json::parse_error &e) {
+		std::cout << "Error reading file:" << std::endl;
+		std::cout << e.what() << std::endl;
 	}
 	return towers;
 }
