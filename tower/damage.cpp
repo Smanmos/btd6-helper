f3f628671450cc990d409a9cc3f77ef561508300
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-15 20:00:01 +0800

    Use default value of 0 in Damage

--- tower/damage.cpp
+++ tower/damage.cpp
@@ -8,7 +8,7 @@ Damage::Damage(int base, int ceramic, int moab) :
 	base(base), ceramic(ceramic), moab(moab) {}
 
 Damage::Damage(json damageJson) {
-	base = damageJson.at("base");
+	base = damageJson.value("base", 0);
 	ceramic = damageJson.value("ceramic", 0);
 	moab = damageJson.value("moab", 0);
 }
