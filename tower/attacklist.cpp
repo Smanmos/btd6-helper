b14fa4701c28cf315c28cb86082fa80e0536846f
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-18 14:30:45 +0800

    Implement AttackList methods

---
+++ tower/attacklist.cpp
@@ -0,0 +1,23 @@
+#include "attacklist.h"
+
+AttackList::AttackList() : attacks(std::vector<Attack>()) {}
+
+AttackList::AttackList(Attack attack) : attacks(std::vector<Attack>{attack}) {}
+
+AttackList::AttackList(AttackList& copy) : attacks(std::vector<Attack>(copy.attacks)) {}
+
+double AttackList::getTotalDps() {
+	double dps = 0;
+	for (auto attack = attacks.begin(); attack != attacks.end(); ++attack) {
+		dps += attack->getDamagePerSecond();
+	}
+	return dps;
+}
+
+AttackList AttackList::improve(Upgrade upgrade) {
+	std::vector<Attack> upgradedAttacks;
+	for (auto attack = attacks.begin(); attack != attacks.end(); ++attack) {
+		upgradedAttacks.push_back(attack->improve(upgrade));
+	}
+	return AttackList(upgradedAttacks);
+}
