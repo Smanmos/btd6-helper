ce1a9d91eae93d9e829eb9cbca1efe2546c25c93
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-18 21:56:31 +0800

    Add JSON constructor to AttacksBuff

--- tower/attacksbuff.cpp
+++ tower/attacksbuff.cpp
@@ -2,6 +2,8 @@
 
 AttacksBuff::AttacksBuff(AttackBuff attackBuff) : attackBuff(attackBuff) {}
 
+AttacksBuff::AttacksBuff(json buffJson) : attackBuff(buffJson) {}
+
 AttackList AttacksBuff::buff(AttackList attacks) {
 	return attacks.improve(attackBuff);
 }
--- tower/attacksbuff.h
+++ tower/attacksbuff.h
@@ -1,10 +1,13 @@
 #ifndef ATTACKS_BUFF
 #define ATTACKS_BUFF
 #include "buff.h"
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 class AttacksBuff : public Buff {
 	AttackBuff attackBuff;
 public:
 	AttacksBuff(AttackBuff);
+	AttacksBuff(json);
 	AttackList buff(AttackList);
 };
 #endif // !ATTACKS_BUFF
