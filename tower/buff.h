618c9d066ec1aab14ec7a0c389b3c3834abd708d
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-21 00:24:52 +0800

    Fix print function in Buff

--- tower/buff.h
+++ tower/buff.h
@@ -7,6 +7,7 @@ public:
 	virtual AttackList buff(AttackList) = 0;
 	friend std::ostream& operator<<(std::ostream& os, Buff& buff) {
 		buff.printToOstream(os);
+		return os;
 	}
 };
 #endif // !BUFF
