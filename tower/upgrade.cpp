2e12f59bda37def466b1c03b7cba7010bfb78648
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-31 00:32:34 +0800

    Use vector of Buffs in Upgrade

--- tower/upgrade.cpp
+++ tower/upgrade.cpp
@@ -1,8 +1,11 @@
 #include "upgrade.h"
 
-Upgrade::Upgrade(json upgradeJson) : internalBuff(createBuff(upgradeJson)) {
+Upgrade::Upgrade(json upgradeJson) {
 	name = upgradeJson.at("name");
 	cost = upgradeJson.at("cost");
+	for (json::iterator buff = upgradeJson["buffs"].begin(); buff != upgradeJson["buffs"].end(); ++buff) {
+		internalBuffs.push_back(createBuff(*buff));
+	}
 }
 
 std::string Upgrade::getName() {
@@ -13,6 +16,6 @@ int Upgrade::getCost() {
 	return cost;
 }
 
-Buff* Upgrade::getBuff() {
-	return internalBuff;
+std::vector<Buff*> Upgrade::getBuffs() {
+	return internalBuffs;
 }
