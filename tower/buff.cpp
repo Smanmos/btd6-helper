c566a159b1e537ef812f92d6469125496b421d07
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-11-02 01:18:00 +0800

    Add ReplaceAttack to createBuff

--- tower/buff.cpp
+++ tower/buff.cpp
@@ -1,6 +1,7 @@
 #include "buff.h"
 #include "attacksbuff.h"
 #include "addattack.h"
+#include "replaceattack.h"
 
 Buff* createBuff(json buffJson) {
 	if (buffJson[0] == "buffAttacks") {
@@ -8,6 +9,9 @@ Buff* createBuff(json buffJson) {
 	}
 	else if (buffJson[0] == "addAttack") {
 		return new AddAttack(buffJson[1]);
+	}
+	else if (buffJson[0] == "replaceAttack") {
+		return new ReplaceAttack(buffJson[1]);
 	}
 	throw std::invalid_argument("Invalid buff json");
 }
