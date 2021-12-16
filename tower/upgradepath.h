81de22d474625461c8525e4de939e63ac8bcbbe5
Smanmos <57220480+Smanmos@users.noreply.github.com>
2021-12-17 00:09:18 +0800

    Add UpgradePath declaration

---
+++ tower/upgradepath.h
@@ -0,0 +1,16 @@
+#ifndef UPGRADE_PATH
+#define UPGRADE_PATH
+#include <string>
+#include <vector>
+#include <nlohmann/json.hpp>
+#include "upgrade.h"
+class UpgradePath {
+private:
+	std::vector<Upgrade> upgrades;
+public:
+	UpgradePath(json upgradePathJson);
+	UpgradePath(std::vector<Upgrade> upgrades);
+	Upgrade getTier(int tier);
+	Upgrade& operator[](int index);
+};
+#endif
