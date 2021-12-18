9d4a6592bf12bb631a210ce11150d60085851b93
Smanmos <57220480+Smanmos@users.noreply.github.com>
2021-12-19 01:47:53 +0800

    Remove buffing in other functions

--- tower/attacklist.cpp
+++ tower/attacklist.cpp
@@ -33,31 +33,13 @@ std::vector<Attack> AttackList::getBuffedAttacks() {
 }
 
 void AttackList::add(Attack attack) {
-	Attack upgradedAttack = attack;
-	for (auto appliedBuff = appliedBuffs.begin(); appliedBuff != appliedBuffs.end(); ++appliedBuff) {
-		AttackBuff buff = appliedBuff->first;
-		std::vector<std::string> *targets = appliedBuff->second;
-		if (targets == nullptr ||
-				std::find(targets->begin(), targets->end(), attack.getName()) != targets->end()) {
-			upgradedAttack = upgradedAttack.improve(buff);
-		}
-	}
-	attacks.push_back(upgradedAttack);
+	attacks.push_back(attack);
 }
 
 void AttackList::replace(std::string target, Attack newAttack) {
 	for (auto attack = attacks.begin(); attack != attacks.end(); ++attack) {
 		if (attack->getName() == target) {
-			Attack upgradedAttack = newAttack;
-			for (auto appliedBuff = appliedBuffs.begin(); appliedBuff != appliedBuffs.end(); ++appliedBuff) {
-				AttackBuff buff = appliedBuff->first;
-				std::vector<std::string>* targets = appliedBuff->second;
-				if (targets == nullptr ||
-					std::find(targets->begin(), targets->end(), newAttack.getName()) != targets->end()) {
-					upgradedAttack = upgradedAttack.improve(buff);
-				}
-			}
-			*attack = upgradedAttack;
+			*attack = newAttack;
 			return;
 		}
 	}
@@ -83,27 +65,14 @@ std::ostream& AttackList::streamStats(std::ostream& os) {
 AttackList AttackList::improve(AttackBuff attackBuff) {
 	std::vector<std::pair<AttackBuff, std::vector<std::string>* > > newBuffs = appliedBuffs;
 	newBuffs.push_back(std::pair<AttackBuff, std::vector<std::string>* >(attackBuff, nullptr));
-	std::vector<Attack> upgradedAttacks;
-	for (auto attack = attacks.begin(); attack != attacks.end(); ++attack) {
-		upgradedAttacks.push_back(attack->improve(attackBuff));
-	}
-	return AttackList(upgradedAttacks, newBuffs);
+	return AttackList(attacks, newBuffs);
 }
 
 AttackList AttackList::improve(AttackBuff attackBuff, std::vector<std::string> targets) {
 	std::vector<std::pair<AttackBuff, std::vector<std::string>* > > newBuffs = appliedBuffs;
 	newBuffs.push_back(std::pair<AttackBuff, std::vector<std::string>* >(attackBuff, 
 		new std::vector<std::string>(targets)));
-	std::vector<Attack> upgradedAttacks;
-	for (auto attack = attacks.begin(); attack != attacks.end(); ++attack) {
-		if (std::find(targets.begin(), targets.end(), attack->getName()) != targets.end()) {
-			upgradedAttacks.push_back(attack->improve(attackBuff));
-		}
-		else {
-			upgradedAttacks.push_back(*attack);
-		}
-	}
-	return AttackList(upgradedAttacks, newBuffs);
+	return AttackList(attacks, newBuffs);
 }
 
 AttackList AttackList::addSubProjOnHit(std::string target, Projectile subProj, int numSubProj) {
